f1e8513c641ed8cba9b7dc5ed0b5cfbff0831a22
KVG24 <141866132+KVG24@users.noreply.github.com>
2025-05-27 11:35:28 +0300

    Add welcome card to Home page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,18 @@ function App() {
         <>
             <NavigationBar />
             <h1>Home Page</h1>
+            <div className="welcome-card">
+                <h2>Welcome to our Fake Store</h2>
+                <p>
+                    We have many fake items in our fake store in different
+                    categories
+                </p>
+                <p>Checkout the Catalogue page</p>
+                <p>
+                    This store is created for the purpose of testing gained
+                    knowledge in different React features and technologies
+                </p>
+            </div>
         </>
     );
 }
